e07d3bffa96a83edce65d53b59989895771c35de
Bassel Amgad <bamgad7@gmail.com>
2022-09-15 18:02:42 +0200

    Initial class.

---
+++ Data/Seedings/101_UserSeed.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace HomeRecipes.Migrations.Seedings
+{
+    [Migration(101)]
+    public class _101_UserSeed : Migration
+    {
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Up()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
