e65df810d511af9f276d3b0669b5bb5a9b1e7c78
Bassel Amgad <bamgad7@gmail.com>
2022-09-15 12:29:36 +0200

    Names for each table

---
+++ Data/Migrations/TableName.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeRecipes.Migrations.Migrations
+{
+    public class TableName
+    {
+        public readonly static string Users = "User";
+        public readonly static string Recipes = "Recipe";
+        public readonly static string Categories = "Category";
+        public readonly static string RecipeCategory = "RecipeCategory";
+    }
+}
