cb3d8a6c2d9007ec302c0b2226b97f3b7ec6f0b1
Bassel Amgad <bamgad7@gmail.com>
2022-10-18 13:27:46 +0200

    Specific Guid

--- Data/Seedings/102_RolesSeed.cs
+++ Data/Seedings/102_RolesSeed.cs
@@ -10,13 +10,15 @@ namespace HomeRecipes.Migrations.Seedings
         {
             Insert.IntoTable(TableName.Roles).Row(new
             {
+                id = new Guid("a0798c89-aa12-455b-8998-b7032dac2fb1"),
                 role = "Admin",
-                isActive = true
+                is_active = true
             });
             Insert.IntoTable(TableName.Roles).Row(new
             {
+                id = new Guid("8f4fd2df-c7b6-48e1-a8a7-6f87ce74d86c"),
                 role = "Guest",
-                isActive = true
+                is_active = true
             });
         }
         public override void Down()
