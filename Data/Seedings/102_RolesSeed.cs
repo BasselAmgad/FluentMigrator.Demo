ebfaec5408891b991eac7b7f1854c5f9efd1ed69
Bassel Amgad <bamgad7@gmail.com>
2022-10-18 12:10:33 +0200

    Seeding Roles table

---
+++ Data/Seedings/102_RolesSeed.cs
@@ -0,0 +1,27 @@
+﻿using FluentMigrator;
+using HomeRecipes.Migrations.Migrations;
+
+namespace HomeRecipes.Migrations.Seedings
+{
+    [Migration(102)]
+    public class _102_RolesSeed : Migration
+    {
+        public override void Up()
+        {
+            Insert.IntoTable(TableName.Roles).Row(new
+            {
+                role = "Admin",
+                isActive = true
+            });
+            Insert.IntoTable(TableName.Roles).Row(new
+            {
+                role = "Guest",
+                isActive = true
+            });
+        }
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
