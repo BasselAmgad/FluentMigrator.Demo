42530561768dba544a009ac1dadc7f0637daad75
Bassel Amgad <bamgad7@gmail.com>
2022-10-19 16:23:10 +0200

    Added Roles

--- ServerJWT/Models/User.cs
+++ ServerJWT/Models/User.cs
@@ -7,12 +7,13 @@ namespace Server.Models
         public Guid Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-
+        public List<string> UserRoles { get; set; }
         public User(string userName, string password)
         {
             Id = Guid.NewGuid();
             UserName = userName;
             Password = password;
+            UserRoles = new();
         }
     }
 }
